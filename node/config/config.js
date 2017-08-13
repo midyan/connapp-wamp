7df0378d3110bf4ccde962a66a63caddccf42b21
yannunes <yannunes@gmail.com>
2017-08-13 16:53:27 -0300

    Config Module added

---
+++ node/config/config.js
@@ -0,0 +1,31 @@
+// Imports and packages
+const path = require('path')
+const w3cws = require('websocket').w3cwebsocket
+
+// Function to get current directory
+const getCurrentDir = () => (path.resolve(__dirname) + '/')
+
+// Directory config object
+const DIR = {
+  ROOT: getCurrentDir().replace('config/', ''),
+  CURRENT: getCurrentDir
+}
+
+// Mongo connection config object
+const MONGO = {
+  URL: 'mongodb://localhost'
+}
+
+// Websocket/WAMP config object
+const WEBSOCKET = {
+  URL: 'ws://localhost:9000/ws',
+  REALM: 'realm1',
+  CLIENT: w3cws
+}
+
+// Config object export
+module.exports = {
+  WEBSOCKET: WEBSOCKET,
+  MONGO: MONGO,
+  DIR: DIR
+}
