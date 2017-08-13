684a98fa50a7e2f2c10fee4756411638ad54ea47
yannunes <yannunes@gmail.com>
2017-08-13 16:33:40 -0300

    Mongo schema example

--- node/lib/mongo/mongo.js
+++ node/lib/mongo/mongo.js
@@ -9,68 +9,9 @@ mongoose.connect(mongoAddress)
 
 //Models
 const models = {
-  History: () => {
-    //Model info
-    const collectionName = 'history'
-    const modelName = 'History'
-    const model =  {
-      user: {
-        type: ObjectId,
-        required: false
-      },
-      solveType: {
-        type: String,
-        required: true,
-        enum: ['mathjs', 'wolfram']
-      },
-      ocrImg: {
-        type: String,
-        required: false,
-      },
-      ocrExp: {
-        type: String,
-        required: false,
-      },
-      expression: {
-        type: String,
-        required: true
-      },
-      query: {
-        type: String,
-        required: true
-      },
-      ocrResult: {
-        type: Mixed,
-        required: false
-      },
-      result: {
-        type: Mixed,
-        required: true
-      },
-      date: {
-        type: Date,
-        default: Date.now
-      },
-      active: {
-        type: Boolean,
-        default: true
-      }
-    }
 
-    //Define Schema
-    const dataSchema = mongoose.Schema(model, {collection: collectionName})
 
-    //Define hooks
-    dataSchema.pre('save', function(next){
-      next()
-    })
-
-    //return Schema and Model Name
-    return {
-      schema: dataSchema,
-      name: modelName
-    }
-  }
+  // #----Example----#
   // User: () => {
   //   //Model info
   //   var collectionName = ''
