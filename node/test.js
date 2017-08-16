1ba48a930939cd26f03c3d26711c2b7216bffd73
Yan Nunes <yannunes@gmail.com>
2017-08-16 18:33:11 +0000

    Not important

--- node/test.js
+++ node/test.js
@@ -1,9 +1,6 @@
 var Wampy = require('wampy').Wampy;
 var w3cws = require('websocket').w3cwebsocket
-var ws = new Wampy('ws://localhost:9000/ws', {ws: w3cws, realm: 'realm1'})
+var ws = new Wampy('ws://localhost:8080/ws', {ws: w3cws, realm: 'realm1'})
 var count = 0
 
-setInterval(function() {
-  count++
-  ws.publish('com.example.counter', [count])
-}, 1000)
+ws.subscribe('com.example.counter', (test) => console.log(test))
