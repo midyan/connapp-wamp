0cbce7dcfa746fa943d8a8ed6385304b9bd23776
yannunes <yannunes@gmail.com>
2017-08-22 08:24:59 -0300

    Add insert and edit test for demonstration

---
+++ node/tests/edit.test.js
@@ -0,0 +1,28 @@
+const
+  INDEX = require('../index.js'),
+  mongo = INDEX.mongo,
+  date = new Date(),
+  day = date.getDate(),
+  month = date.getMonth() + 1,
+  year = date.getFullYear(),
+  dateString = [day, month, year].join('/'),
+  hour = date.getHours().toString(),
+  minute = date.getMinutes().toString()
+
+if (hour < 10) hour = '0' + hour
+if (minute < 10) minute = '0' + minute
+
+const hours = [hour, minute].join(':')
+const timestamp = dateString + ' ' + hours
+
+const query = {
+  titulo: 'Essa noticia vai ser editada'
+}
+
+mongo.models.fakenews.findOne(query)
+  .then(res => {
+    res.title = 'O Título foi editado em ' + timestamp
+    return res.save().exec()
+  })
+  .then(res => console.log('Notícia foi editada com sucesso'))
+  .catch(e => console.log(e))
