f8e0bd2425cbd5864256e3cd6ddebaca26afaf3e
yannunes <yannunes@gmail.com>
2017-08-22 08:26:35 -0300

    More add to test

---
+++ node/tests/insert.toedit.test.js
@@ -0,0 +1,30 @@
+const
+  INDEX = require('../index.js'),
+  mongo = INDEX.mongo,
+  date = new Date(),
+  day = date.getDate(),
+  month = date.getMonth() + 1,
+  year = date.getFullYear(),
+  dateString = [day, month, year].join('/'),
+  hour = date.getHours().toString(),
+  minute = date.getMinutes().toString()
+
+if (hour < 10) hour = '0' + hour
+if (minute < 10) minute = '0' + minute
+
+const hours = [hour, minute].join(':')
+const timestamp = dateString + ' ' + hours
+
+const fakenews = {
+  title: 'Essa noticia vai ser editada',
+  body: `Essa notícia foi adicionada em ${timestamp}`
+}
+
+const CNN = new mongo.models.fakenews(fakenews)
+CNN.isNew = true
+
+CNN.save()
+  .then(res => {
+    console.log('Notícia nova adicionada com sucesso')
+  })
+  .catch(e => console.log(e))
