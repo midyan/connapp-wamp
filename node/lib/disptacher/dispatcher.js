42f48b990d7873560fd70069c2716e3cc09c8a07
yannunes <yannunes@gmail.com>
2017-09-16 19:10:58 -0300

    length

--- node/lib/disptacher/dispatcher.js
+++ node/lib/disptacher/dispatcher.js
@@ -14,17 +14,17 @@ const ws = INDEX.connection
  * @param  {String} _id       ID of the documento to perform publish
  * @param  {Object} [data={}] Updated data to send
  */
-const insertToApp = (model, data = {}) => {
+const insertToApp = (model, data = {}, length = undefined) => {
   // Builds event string
   const event = `connapp.app.${model.toLowerCase()}.insert`
 
   // Publish the event
   ws.publish(
     event,
-    [data]
+    [data, length]
   )
 
- //  console.log(event + ' was published')
+  console.log(event + ' was published ' + length)
 }
 
 // FUnction to tell the App to update local documento o model
