0af7fd43bb42a83ebf604e2fd5e08a303277dfdf
yannunes <yannunes@gmail.com>
2017-09-18 10:19:20 -0300

    Update tst for event

---
+++ node/tests/update.event.test.js
@@ -0,0 +1,30 @@
+var
+  INDEX = require('../index.js'),
+  mongo = INDEX.mongo,
+  date = new Date(),
+  day = date.getDate(),
+  month = date.getMonth() + 1,
+  year = date.getFullYear(),
+  dateString = [day, month, year].join('/'),
+  hour = date.getHours().toString(),
+  minute = date.getMinutes().toString(),
+  seconds = date.getSeconds().toString()
+
+if (hour < 10) hour = '0' + hour
+if (minute < 10) minute = '0' + minute
+if (seconds < 10) seconds = '0' + seconds
+
+const hours = [hour, minute, seconds].join(':')
+const timestamp = dateString + ' ' + hours
+
+const query = {
+  _id: '59bc2fce3d63679b77fe6cde'
+}
+
+mongo.models.event.findOne(query)
+  .then(res => {
+    res.Nam = 'O Título foi editado!!!  Realizado em: ' + timestamp
+    return res.save()
+  })
+  .then(res => console.log('Notícia foi editada com sucesso'))
+  .catch(e => console.log(e))
