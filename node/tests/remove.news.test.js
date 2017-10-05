e9af1227263b01f0f7dd676030e2909cacc21fd4
yannunes <yannunes@gmail.com>
2017-10-05 13:47:52 -0300

    logicalRemove test news script

---
+++ node/tests/remove.news.test.js
@@ -0,0 +1,32 @@
+var
+  INDEX = require('../index.js'),
+  mongo = INDEX.mongo,
+  date = new Date(),
+  day = date.getDate(),
+  month = date.getMonth() + 1,
+  year = date.getFullYear(),
+  dateString = [day, month, year].join('/'),
+  hour = date.getHours().toString(),
+  minute = date.getMinutes().toString(),
+  seconds = date.getSeconds().toString()
+
+if (month < 10) month = '0' + month
+if (day < 10) day = '0' + day
+if (hour < 10) hour = '0' + hour
+if (minute < 10) minute = '0' + minute
+if (seconds < 10) seconds = '0' + seconds
+
+const hours = [hour, minute, seconds].join(':')
+const timestamp = dateString + ' ' + hours
+
+newsModel.isNew = true
+
+newsModel.findOne({})
+  .then(res => {
+    const _id = res._id
+    return newsModel.update({_id}, { $set: { active: false } })
+  })
+  .then(res => {
+    console.log('Logical Remove com sucesso')
+  })
+  .catch(e => console.log(e))
