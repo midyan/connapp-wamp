0de6698cdb06e6a5a59336eddebde42ce8ad8a9d
yannunes <yannunes@gmail.com>
2017-10-06 02:09:42 -0300

    Mongo is according to cms

--- node/lib/mongo/mongo.js
+++ node/lib/mongo/mongo.js
@@ -362,6 +362,76 @@ const models = {
       schema: dataSchema,
       name: modelName
     }
+  },
+  news: () => {
+    //Model info
+    const collectionName = 'news'
+    const modelName = 'news'
+    const model =  {
+      /**
+      * Titulo da notícia.
+      * @type {String}
+      */
+      title: {
+        type: String,
+        required: true
+      },
+      /**
+      * Mensagem da notícia
+      * @type {String}
+      */
+      message: {
+        type: String,
+        required: true
+      },
+      /**
+      * Capa da notícia.
+      * @type {String}
+      */
+      cover: {
+        type: String
+      },
+      /**
+      * Define se o notícia está ativo.
+      * @type {Boolean}
+      */
+      active: {
+        type: Boolean,
+        default: true
+      },
+      /**
+      * Data de criação do notícia.
+      * @type {Date}
+      */
+      createAt: {
+        type: Date,
+        default: Date.now
+      },
+      /**
+      * Data da última vez que os dados do notícia foi atualizado.
+      * @type {String}
+      */
+      lastUpdate: {
+        type: Date,
+        default: Date.now
+      }
+    }
+
+    //Define Schema
+    const dataSchema = mongoose.Schema(model, {collection: collectionName})
+
+    //Define hooks
+    dataSchema.pre('save', function(next){
+      this.lastUpdated = new Date()
+      dispatchOnSave(this)
+      next()
+    })
+
+    //return Schema and Model Name
+    return {
+      schema: dataSchema,
+      name: modelName
+    }
   }
 }
 
