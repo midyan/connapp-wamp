3da4d64c85c629bcb04f25f893d2884d567920a1
yannunes <yannunes@gmail.com>
2017-10-05 23:52:13 -0300

    Mongo is according to cms

--- node/lib/mongo/mongo.js
+++ node/lib/mongo/mongo.js
@@ -15,6 +15,18 @@ const MONGO = INDEX.CONFIG.MONGO
 // Connect to Mongo
 mongoose.connect(MONGO.URL)
 
+const dispatchOnSave = (doc) => {
+  const _id = doc._id.toString(),
+        data = _.cloneDeep(doc)
+
+  // Dispatches for realtimeUpdate
+  if (doc.isNew) {
+    dispatcher.insertToApp(modelName, data)
+  } else {
+    dispatcher.updateDocumentToApp(modelName, _id, data)
+  }
+}
+
 //Models
 const models = {
   events: () => {
@@ -22,98 +34,100 @@ const models = {
     const collectionName = 'events'
     const modelName = 'events'
     const model =  {
+      /**
+      * Nome do evento.
+      * @type {String}
+      */
       name: {
         type: String,
         required: true
       },
+      /**
+      * Descrição do evento
+      * @type {String}
+      */
+      description: {
+        type: String,
+        required: true
+      },
+      /**
+      * Data e hora que o evento irá começar.
+      * @type {Date}
+      */
       start: {
         type: Date,
         required: true
       },
+      /**
+      * Data e hora que o evento irá terminar.
+      * @type {Date}
+      */
       end: {
         type: Date,
         required: true
       },
-      local: {
-        type: ObjectId
-      },
-      order: {
+      /**
+      * Quantidade de pessoas que gostaram do evento.
+      * @type {Number}
+      */
+      like: {
         type: Number,
-        required: false
+        default: 0
       },
-      eventType: {
-        type: ObjectId
-      },
+      /**
+      * Palestrantes que irão ministrar o evento.
+      * @type {Array}
+      */
       speakers: [{
-        type: ObjectId
+        type: ObjectId,
+        ref: 'speakers'
       }],
-      likes: {
-        type: Number,
-        default: 0,
-        required: false
-      },
-      active: {
-        type: Boolean,
-        default: true
-      },
-      lastUpdate: {
-        type: Date,
-        default: Date.now,
-      },
-      createAt: {
-        type: Date,
-        default: Date.now,
-      },
-    }
-
-    //Define Schema
-    const dataSchema = mongoose.Schema(model, {collection: collectionName})
-
-    //Defines Pre-Save hook
-    dataSchema.pre('save', function(next){
-      // Defines arguments for dispatch function
-      this.lastUpdate = new Date()
-
-      const _id = this._id.toString(),
-            data = _.cloneDeep(this)
-
-      // Dispatches for realtimeUpdate
-      if (this.isNew) {
-        dispatcher.insertToApp(modelName, data)
-      } else {
-        dispatcher.updateDocumentToApp(modelName, _id, data)
-      }
-
-      next()
-    })
-
-    //return Schema and Model Name
-    return {
-      schema: dataSchema,
-      name: modelName
-    }
-  },
-  locals: () => {
-    //Model info
-    const collectionName = 'locals'
-    const modelName = 'locals'
-    const model =  {
-      name: {
-        type: String,
+      /**
+      * Tipo de evento
+      * @type {ObjectId}
+      */
+      eventType: {
+        type: ObjectId,
+        ref: 'eventtypes',
         required: true
       },
-      mapImage: {
-        type: String,
-        required: false
+      /*
+      * Ordem de precedência do evento
+      * @type {Number}
+      */
+      order: {
+        type: Number,
+        default: 0
       },
+      /**
+      * Local onde o evento será realizado.
+      * @type {ObjectId}
+      */
+      place: {
+        type: ObjectId,
+        ref: 'places',
+        required: true
+      },
+      /**
+      * Define se o evento está ativo.
+      * @type {Boolean}
+      */
       active: {
         type: Boolean,
         default: true
       },
+      /**
+      * Data de criação do evento.
+      * @type {Date}
+      */
       createAt: {
         type: Date,
         default: Date.now
       },
+      /**
+      * Data da última vez que os dados do evento foi atualizado.
+      * @type {String}
+      */
       lastUpdate: {
         type: Date,
         default: Date.now
@@ -125,19 +139,70 @@ const models = {
 
     //Define hooks
     dataSchema.pre('save', function(next){
-      // Defines arguments for dispatch function
-      this.lastUpdate = new Date()
+      this.lastUpdated = new Date()
+      dispatchOnSave(this)
+      next()
+    })
 
-      const _id = this._id.toString(),
-            data = _.cloneDeep(this)
-
-      // Dispatches for realtimeUpdate
-      if (this.isNew) {
-        dispatcher.insertToApp(modelName, data)
-      } else {
-        dispatcher.updateDocumentToApp(modelName, _id, data)
+    //return Schema and Model Name
+    return {
+      schema: dataSchema,
+      name: modelName
+    }
+  },
+  places: () => {
+    //Model info
+    const collectionName = 'places'
+    const modelName = 'places'
+    const model =  {
+      /**
+        * Nome do local.
+        * @type {String}
+        */
+      name: {
+        type: String,
+        required: true
+      },
+      /**
+        * Imagem com o mapa do local.
+        * @type {String}
+        */
+      mapImage: {
+        type: String
+      },
+      /**
+        * Define se o local está ativo.
+        * @type {Boolean}
+        */
+      active: {
+        type: Boolean,
+        default: true
+      },
+      /**
+      * Data de criação do local.
+      * @type {Date}
+      */
+      createAt: {
+        type: Date,
+        default: Date.now
+      },
+      /**
+       * Data da última vez que os dados do local foi atualizado.
+       * @type {String}
+       */
+      lastUpdate: {
+        type: Date,
+        default: Date.now
       }
+    }
 
+    //Define Schema
+    const dataSchema = mongoose.Schema(model, {collection: collectionName})
+
+    //Define hooks
+    dataSchema.pre('save', function(next){
+      this.lastUpdated = new Date()
+      dispatchOnSave(this)
       next()
     })
 
@@ -152,18 +217,34 @@ const models = {
     const collectionName = 'eventtypes'
     const modelName = 'eventtypes'
     const model =  {
+      /**
+        * Nome do eventType.
+        * @type {String}
+        */
       name: {
         type: String,
         required: true
       },
+      /**
+        * Define se o eventType está ativo.
+        * @type {Boolean}
+        */
       active: {
         type: Boolean,
         default: true
       },
+      /**
+        * Data de criação do eventType.
+        * @type {Date}
+        */
       createAt: {
         type: Date,
         default: Date.now
       },
+      /**
+        * Data da última vez que os dados do eventType foi atualizado.
+        * @type {String}
+        */
       lastUpdate: {
         type: Date,
         default: Date.now
@@ -175,99 +256,8 @@ const models = {
 
     //Define hooks
     dataSchema.pre('save', function(next){
-      // Defines arguments for dispatch function
-      this.lastUpdate = new Date()
-
-      const _id = this._id.toString(),
-            data = _.cloneDeep(this)
-
-      // Dispatches for realtimeUpdate
-      if (this.isNew) {
-        dispatcher.insertToApp(modelName, data)
-      } else {
-        dispatcher.updateDocumentToApp(modelName, _id, data)
-      }
-
-      next()
-    })
-
-    //return Schema and Model Name
-    return {
-      schema: dataSchema,
-      name: modelName
-    }
-  },
-  news: () => {
-    //Model info
-    const collectionName = 'news'
-    const modelName = 'news'
-    const model =  {
-     /**
-      * Titulo da notícia.
-      * @type {String}
-      */
-      title: {
-        type: String,
-        required: true
-      },
-      /**
-      * Mensagem da notícia
-      * @type {String}
-      */
-      message: {
-        type: String,
-        required: true
-      },
-      /**
-      * Capa da notícia.
-      * @type {String}
-      */
-      cover: {
-        type: String
-      },
-      /**
-      * Define se o notícia está ativo.
-      * @type {Boolean}
-      */
-      active: {
-        type: Boolean,
-        default: true
-      },
-      /**
-      * Data de criação do notícia.
-      * @type {Date}
-      */
-      createAt: {
-        type: Date,
-        default: Date.now
-      },
-      /**
-      * Data da última vez que os dados do notícia foi atualizado.
-      * @type {String}
-      */
-      lastUpdate: {
-        type: Date,
-        default: Date.now
-      }
-    }
-    //Define Schema
-    const dataSchema = mongoose.Schema(model, {collection: collectionName})
-
-    //Define hooks
-    dataSchema.pre('save', function(next){
-      // Defines arguments for dispatch function
-      this.lastUpdate = new Date()
-
-      const _id = this._id.toString(),
-            data = _.cloneDeep(this)
-
-      // Dispatches for realtimeUpdate
-      if (this.isNew) {
-        dispatcher.insertToApp(modelName, data)
-      } else {
-        dispatcher.updateDocumentToApp(modelName, _id, data)
-      }
-
+      this.lastUpdated = new Date()
+      dispatchOnSave(this)
       next()
     })
 
@@ -282,14 +272,18 @@ const models = {
     const collectionName = 'speakers'
     const modelName = 'speakers'
     const model =  {
+      /**
+      * Nome do palestrante.
+      * @type {String}
+      */
       name: {
         type: String,
         required: true
       },
-      institution: {
-        type: String,
-        required: false
-      },
+      /**
+      * Fotos do palestrante
+      * @type {Object}
+      */
       image: {
         perfil: {
           type: String
@@ -299,23 +293,54 @@ const models = {
         },
         type: Object
       },
+      /**
+      * Profissão do palestrante.
+      * @type {String}
+      */
       profession: {
         type: String
       },
+      /**
+      * ??
+      * @type {String}
+      */
+      institution: {
+        type: String
+      },
+      /**
+      * Descrição sobre o palestrante
+      * @type {String}
+      */
       about: {
         type: String
       },
+      /**
+      * Link para meios de comunicação do palestrante
+      * @type {String}
+      */
       media: {
         type: String
       },
+      /**
+      * Define se o palestrante está ativo.
+      * @type {Boolean}
+      */
       active: {
         type: Boolean,
         default: true
       },
+      /**
+      * Data de criação do palestrante.
+      * @type {Date}
+      */
       createAt: {
         type: Date,
         default: Date.now
       },
+      /**
+      * Data da última vez que os dados do palestrante foi atualizado.
+      * @type {String}
+      */
       lastUpdate: {
         type: Date,
         default: Date.now
@@ -327,19 +352,8 @@ const models = {
 
     //Define hooks
     dataSchema.pre('save', function(next){
-      // Defines arguments for dispatch function
-      this.lastUpdate = new Date()
-
-      const _id = this._id.toString(),
-            data = _.cloneDeep(this)
-
-      // Dispatches for realtimeUpdate
-      if (this.isNew) {
-        dispatcher.insertToApp(modelName, data)
-      } else {
-        dispatcher.updateDocumentToApp(modelName, _id, data)
-      }
-
+      this.lastUpdated = new Date()
+      dispatchOnSave(this)
       next()
     })
 
@@ -349,8 +363,6 @@ const models = {
       name: modelName
     }
   }
-
-
 }
 
 // Export functon, to compile all models
