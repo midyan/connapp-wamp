74d222ff388135b37c16ef1f03524f09c0c257cb
yannunes <yannunes@gmail.com>
2017-10-06 03:13:04 -0300

    Edit info test

---
+++ node/tests/edit.info.test.js
@@ -0,0 +1,33 @@
+let
+  INDEX = require('../index.js'),
+  mongo = INDEX.mongo,
+  date = new Date(),
+  day = date.getDate(),
+  month = date.getMonth() + 1,
+  year = date.getFullYear(),
+  hour = date.getHours().toString(),
+  minute = date.getMinutes().toString(),
+  seconds = date.getSeconds().toString()
+
+if (month < 10) month = '0' + month
+if (day < 10) day = '0' + day
+if (hour < 10) hour = '0' + hour
+if (minute < 10) minute = '0' + minute
+if (seconds < 10) seconds = '0' + seconds
+
+let dateString = [day, month, year].join('/')
+
+const hours = [hour, minute, seconds].join(':')
+const timestamp = dateString + ' ' + hours
+const infoModel = mongo.models.info
+
+infoModel.findOne({active: true})
+  .then(res => {
+    res.title = 'hahahahahahahahahahahahahahhahaha'
+    res.isNew = false
+    return res.save()
+  })
+  .then(res => {
+    console.log('Logical Remove com sucesso')
+  })
+  .catch(e => console.log(e))
