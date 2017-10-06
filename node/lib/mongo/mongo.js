3e518ce891c1427eb714442d62d8d43f11045c3e
yannunes <yannunes@gmail.com>
2017-10-06 02:17:14 -0300

    Add info collection

--- node/lib/mongo/mongo.js
+++ node/lib/mongo/mongo.js
@@ -432,6 +432,69 @@ const models = {
       schema: dataSchema,
       name: modelName
     }
+  },
+  info: () => {
+    //Model info
+    const collectionName = 'info'
+    const modelName = 'info'
+    const model =  {
+      /**
+      * Titulo da informação.
+      * @type {String}
+      */
+      title: {
+        type: String,
+        required: true
+      },
+      /**
+      * Mensagem da informação
+      * @type {String}
+      */
+      message: {
+        type: String,
+        required: true
+      },
+      /**
+      * Define se o informação está ativo.
+      * @type {Boolean}
+      */
+      active: {
+        type: Boolean,
+        default: true
+      },
+      /**
+      * Data de criação do informação.
+      * @type {Date}
+      */
+      createAt: {
+        type: Date,
+        default: Date.now
+      },
+      /**
+      * Data da última vez que os dados do informação foi atualizado.
+      * @type {String}
+      */
+      lastUpdate: {
+        type: Date,
+        default: Date.now
+      }
+    }
+
+    //Define Schema
+    const dataSchema = mongoose.Schema(model, {collection: collectionName})
+
+    //Define hooks
+    dataSchema.pre('save', function(next){
+      this.lastUpdated = new Date()
+      dispatchOnSave(this, modelName)
+      next()
+    })
+
+    //return Schema and Model Name
+    return {
+      schema: dataSchema,
+      name: modelName
+    }
   }
 }
 
