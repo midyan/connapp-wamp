d98684eebb5100623ea775afa76658e021b1d55f
yannunes <yannunes@gmail.com>
2017-10-08 14:15:12 -0300

    Bigger batch

--- node/lib/watcher/watcher.js
+++ node/lib/watcher/watcher.js
@@ -110,7 +110,7 @@ const watchSync = () => {
             })
 
             if (insertData.length) {
-              _.chunk(insertData, 110).forEach(item => {
+              _.chunk(insertData, 500).forEach(item => {
                 dispatcher.insertToApp(model, item, data.length, mobileSession)
               })
             }
